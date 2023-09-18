7c57e983307213afe9a428fc11e2612becfc1168
Shroominic <db2002dominic@gmail.com>
2023-09-18 22:09:05 +0200

    📝 Update funcchain/chain.py file

--- funcchain/chain.py
+++ funcchain/chain.py
@@ -1,26 +1,76 @@
-from typing import TypeVar
+from typing import TypeVar, Any
 
 from langchain.pydantic_v1 import BaseModel
 from langchain.callbacks import get_openai_callback
 from langchain.schema import BaseMessage, BaseOutputParser
+from langchain.schema.runnable import RunnableSequence
 from langchain.output_parsers.openai_functions import PydanticOutputFunctionsParser
-from funcchain import settings
+from funcchain.config import settings
 from funcchain.parser import ParserBaseModel
 from funcchain.prompt import create_prompt
 from funcchain.utils import (
-    create_long_llm,
     from_docstring,
-    gather_llm_type,
+    is_function_model,
     get_parent_frame,
     get_output_type,
     kwargs_from_parent,
     pydantic_to_functions,
     log,
     parser_for,
-    retry_parse,
+    retry_parse
 )
 
 T = TypeVar("T")
+
+
+def create_chain(
+    instruction: str | None = None,
+    system: str = settings.DEFAULT_SYSTEM_PROMPT,
+    parser: BaseOutputParser[T] | None = None,
+    context: list[BaseMessage] = [],
+    input_kwargs: dict[str, Any] = {},
+) -> RunnableSequence:
+    from funcchain import LLM
+    output_type = get_output_type()
+    instruction = instruction or from_docstring()
+    parser = parser or parser_for(output_type)
+    input_kwargs.update(kwargs_from_parent())
+    
+    _add_format_instructions(parser, instruction, input_kwargs)
+    
+    prompt = create_prompt(instruction, system, context, **input_kwargs)
+    
+    if is_function_model(LLM):
+        if issubclass(output_type, BaseModel) and not issubclass(
+            output_type, ParserBaseModel
+        ):
+            input_kwargs[
+                "format_instructions"
+            ] = f"Extract to {output_type.__name__}."
+            functions = pydantic_to_functions(output_type)
+            if hasattr(LLM, "runnable"):
+                LLM = LLM.runnable.bind(**functions).with_fallbacks(
+                    [fallback.bind(**functions) for fallback in LLM.fallbacks if hasattr(LLM, "fallbacks")]
+                )
+            return (
+                prompt
+                | LLM
+                | PydanticOutputFunctionsParser(pydantic_schema=output_type)
+            )
+    return (prompt | LLM | parser)
+
+
+def _add_format_instructions(
+    parser: BaseOutputParser,
+    instruction: str,
+    input_kwargs: dict,
+) -> None:
+    try:
+        if format_instructions := parser.get_format_instructions():
+            instruction += "\n\n{format_instructions}"
+            input_kwargs["format_instructions"] = format_instructions
+    except NotImplementedError:
+        pass
 
 
 @retry_parse(3)
@@ -30,66 +80,18 @@ def chain(
     parser: BaseOutputParser[T] | None = None,
     context: list[BaseMessage] = [],
     **input_kwargs,
-) -> T:  # type: ignore
+) -> T:
     """
     Get response from chatgpt for provided instructions.
     """
-    output_type = get_output_type()
-    chain_name = get_parent_frame(3).function
-    if instruction is None:
-        instruction = from_docstring()
-    if parser is None:
-        parser = parser_for(output_type)
-    input_kwargs.update(kwargs_from_parent())
+    with get_openai_callback() as cb:    
+        chain = create_chain(
+            instruction, system, parser, context, input_kwargs
+        ).invoke(input_kwargs)
+        if cb.total_tokens != 0:
+            log(f"{cb.total_tokens:05}T / {cb.total_cost:.3f}$ - {get_parent_frame(4).function}")
+    return chain
 
-    try:
-        if format_instructions := parser.get_format_instructions():
-            instruction += "\n\n{format_instructions}"
-            input_kwargs["format_instructions"] = format_instructions
-    except NotImplementedError:
-        pass
-
-    prompt = create_prompt(instruction, system, context, **input_kwargs)
-    llm = create_long_llm()
-
-    match gather_llm_type(llm):
-        case "base_model":
-            raise NotImplementedError
-
-        case "chat_model":
-            result = (prompt | llm | parser).invoke(input_kwargs)
-            log(f"N/A token - {chain_name}")
-
-        case "openai_model":
-            with get_openai_callback() as cb:
-                result = (prompt | llm | parser).invoke(input_kwargs)
-                log(f"{cb.total_tokens:05}T / {cb.total_cost:.3f}$ - {chain_name}")
-
-        case "function_model":
-            if issubclass(output_type, BaseModel) and not issubclass(
-                output_type, ParserBaseModel
-            ):
-                input_kwargs[
-                    "format_instructions"
-                ] = f"Extract to {output_type.__name__}."
-                functions = pydantic_to_functions(output_type)
-                llm = llm.runnable.bind(**functions).with_fallbacks(
-                    [fallback.bind(**functions) for fallback in llm.fallbacks]
-                )
-                with get_openai_callback() as cb:
-                    result = (
-                        prompt
-                        | llm
-                        | PydanticOutputFunctionsParser(pydantic_schema=output_type)
-                    ).invoke(input_kwargs)
-                    log(
-                        f"{cb.total_tokens:05}T / {cb.total_cost:.3f}$ - {chain_name} - function_call"
-                    )
-            else:
-                with get_openai_callback() as cb:
-                    result = (prompt | llm | parser).invoke(input_kwargs)
-                    log(f"{cb.total_tokens:05}T / {cb.total_cost:.3f}$ - {chain_name}")
-    return result
 
 
 @retry_parse(3)
@@ -103,56 +105,10 @@ async def achain(
     """
     Get response from chatgpt for provided instructions.
     """
-    output_type = get_output_type()
-    chain_name = get_parent_frame(3).function
-    if instruction is None:
-        instruction = from_docstring()
-    if parser is None:
-        parser = parser_for(output_type)
-    input_kwargs.update(kwargs_from_parent())
-
-    try:
-        if format_instructions := parser.get_format_instructions():
-            instruction += "\n\n{format_instructions}"
-            input_kwargs["format_instructions"] = format_instructions
-    except NotImplementedError:
-        pass
-
-    prompt = create_prompt(instruction, system, context, **input_kwargs)
-    llm = create_long_llm()
-
-    match gather_llm_type(llm):
-        case "base_model":
-            raise NotImplementedError
-        case "chat_model":
-            result = await (prompt | llm | parser).ainvoke(input_kwargs)
-            log(f"N/A token - {chain_name}")
-        case "openai_model":
-            with get_openai_callback() as cb:
-                result = await (prompt | llm | parser).ainvoke(input_kwargs)
-                log(f"{cb.total_tokens:05}T / {cb.total_cost:.3f}$ - {chain_name}")
-        case "function_model":
-            if issubclass(output_type, BaseModel) and not issubclass(
-                output_type, ParserBaseModel
-            ):
-                input_kwargs[
-                    "format_instructions"
-                ] = f"Extract to {output_type.__name__}."
-                functions = pydantic_to_functions(output_type)
-                llm = llm.runnable.bind(**functions).with_fallbacks(
-                    [fallback.bind(**functions) for fallback in llm.fallbacks]
-                )
-                with get_openai_callback() as cb:
-                    result = await (
-                        prompt
-                        | llm
-                        | PydanticOutputFunctionsParser(pydantic_schema=output_type)
-                    ).ainvoke(input_kwargs)
-                    log(
-                        f"{cb.total_tokens:05}T / {cb.total_cost:.3f}$ - {chain_name} - function_call"
-                    )
-            else:
-                with get_openai_callback() as cb:
-                    result = await (prompt | llm | parser).ainvoke(input_kwargs)
-                    log(f"{cb.total_tokens:05}T / {cb.total_cost:.3f}$ - {chain_name}")
-    return result
+    with get_openai_callback() as cb:    
+        chain = await create_chain(
+            instruction, system, parser, context, input_kwargs
+        ).ainvoke(input_kwargs)
+        if cb.total_tokens != 0:
+            log(f"{cb.total_tokens:05}T / {cb.total_cost:.3f}$ - {get_parent_frame(4).function}")
+    return chain
