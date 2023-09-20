124390d51fb9e496673ecb588b60e0937bba453e
Shroominic <db2002dominic@gmail.com>
2023-09-20 12:51:21 +0200

    📁 Update funcchain/__init__.py file

--- funcchain/__init__.py
+++ funcchain/__init__.py
@@ -1,12 +1,10 @@
 from funcchain.config import settings
 from funcchain.chain import chain, achain
 from langchain.pydantic_v1 import BaseModel
-from funcchain.utils.model_defaults import LLM
 
 __all__ = [
     "settings",
     "chain",
     "achain",
     "BaseModel",
-    "MODEL",
 ]
