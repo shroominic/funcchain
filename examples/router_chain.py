cdb8adc155018a6072f29ea149dfffa813faecc1
Shroominic <db2002dominic@gmail.com>
2023-09-23 21:42:52 +0200

    🔀 router improvements

--- examples/router_chain.py
+++ examples/router_chain.py
@@ -4,50 +4,41 @@ from funcchain import chain, settings
 from langchain.pydantic_v1 import BaseModel, Field, validator
 
 
-
-settings.OPENAI_API_KEY = "sk-*******"
-
-
-def handle_pdf_requests() -> None:
-    print("Handling pdf requests")
+def handle_pdf_requests(user_query: str) -> None:
+    print("Handling pdf requests with user query: ", user_query)
 
 
-def handle_csv_requests() -> None:
-    print("Handling csv requests")
+def handle_csv_requests(user_query: str) -> None:
+    print("Handling csv requests with user query: ", user_query)
 
 
-def handle_normal_answer_requests() -> Any:
-    print("Handling normal answer requests")
+def handle_normal_answer_requests(user_query: str) -> Any:
+    print("Handling normal answer requests with user query: ", user_query)
 
 
-ROUTES = {
-    "pdf": handle_pdf_requests,
-    "csv": handle_csv_requests,
-    "normal answer": handle_normal_answer_requests,
-}
-
-class Routes(Enum):
-    pdf = "handle_pdf_requests"
-    csv = "handle_csv_requests"
-    normal_answer = "handle_normal_answer_requests"
-
-
-class Route(BaseModel):
-    selector: str = Field(
-        ..., description=f"Select one of the following options {ROUTES.keys()}"
-    )
-
-    def __call__(self, *args, **kwargs) -> Any:
-        return ROUTES[self.selector](*args, **kwargs)
+class Routes(str, Enum):
+    pdf = "pdf"
+    csv = "csv"
+    normal_answer = "normal_answer"
+    
+    def __call__(self, user_query: str) -> Any:
+        match self.value:
+            case self.pdf:
+                return handle_pdf_requests(user_query)
+            case self.csv:
+                return handle_csv_requests(user_query)
+            case self.normal_answer:
+                return handle_normal_answer_requests(user_query)
 
 
-def route_query(user_query: str, routes: str) -> Route:
+class Router(BaseModel):
+    selector: Routes = Field(..., description="Enum of the available routes.")
+
+
+def route_query(user_query: str) -> Router:
     """
     USER_QUERY:
     {user_query}
-
-    AVAILABLE ROUTES:
-    {routes}
 
     Given a user query select the best query handler for it.
     """
@@ -57,6 +48,6 @@ def route_query(user_query: str, routes: str) -> Route:
 if __name__ == "__main__":
     user_query = input("Enter your query: ")
 
-    routed_chain = route_query(user_query, ROUTES.keys())
+    routed_chain = route_query(user_query)
 
-    routed_chain()
+    routed_chain.selector(user_query)
