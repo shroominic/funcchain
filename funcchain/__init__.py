f35aff1ebbdaa0f75c37832970d18790cbd917d9
Shroominic <db2002dominic@gmail.com>
2023-11-08 22:20:36 +0100

    ✨ Updated import statements in __init__.py

--- funcchain/__init__.py
+++ funcchain/__init__.py
@@ -1,11 +1,14 @@
 from langchain.pydantic_v1 import BaseModel
 
-from funcchain.chain import achain, chain
+from funcchain.chain import Chain as achain
+from funcchain.chain import Chain as chain
 from funcchain.config import settings
+from funcchain.types import Error
 
 __all__ = [
     "settings",
     "chain",
     "achain",
     "BaseModel",
+    "Error",
 ]
