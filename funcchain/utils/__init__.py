396f124c56eaf8675f87d57a38507ebefe278342
Shroominic <db2002dominic@gmail.com>
2023-11-09 23:44:35 +0100

    🌌 image utils

--- funcchain/utils/__init__.py
+++ funcchain/utils/__init__.py
@@ -1,3 +1,4 @@
 from funcchain.utils.function_frame import *  # noqa: F401, F403
 from funcchain.utils.helpers import *  # noqa: F401, F403
 from funcchain.utils.model_defaults import *  # noqa: F401, F403
+from funcchain.utils.image_helpers import *  # noqa: F401, F403
---
+++ funcchain/utils/image_helpers.py
@@ -0,0 +1,10 @@
+from io import BytesIO
+from base64 import b64encode
+from PIL import Image  # type: ignore
+
+
+def image_to_base64_url(image: Image.Image) -> str:
+    with BytesIO() as output:
+        image.save(output, format="PNG")
+        base64_image = b64encode(output.getvalue()).decode("utf-8")
+    return f"data:image/jpeg;base64,{base64_image}"
