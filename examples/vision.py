38e74803e867cb57c64a0d23ff0e313ae44eda58
Shroominic <db2002dominic@gmail.com>
2023-11-09 23:44:03 +0100

    👀 vision example

--- examples/vision.py
+++ examples/vision.py
@@ -1,1 +1,32 @@
-# coming soon
+from funcchain import chain, settings
+from langchain.pydantic_v1 import BaseModel, Field
+from PIL import Image  # type: ignore
+
+settings.MODEL_NAME = "gpt-4-vision-preview"
+
+
+class AnalysisResult(BaseModel):
+    """The result of an image analysis."""
+    theme: str = Field(..., description="The theme of the image")
+    description: str = Field(..., description="A description of the image")
+    objects: list[str] = Field(..., description="A list of objects found in the image")
+
+
+def analyse_image(image: Image.Image) -> AnalysisResult:
+    """
+    Analyse the image and extract its
+    theme, description and objects.
+    """
+    return chain()
+
+
+if __name__ == "__main__":
+    example_image = Image.open("examples/cyberspace_landscape.jpg")
+    
+    result = analyse_image(example_image)
+    
+    print("Theme:", result.theme)
+    print("Description:", result.description)
+
+    for obj in result.objects:
+        print("Found this object:", obj)
