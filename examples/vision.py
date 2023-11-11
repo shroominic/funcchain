2b6180eded1f3b00a3d63f62b7a06bdf54bd7b11
Shroominic <db2002dominic@gmail.com>
2023-11-11 13:25:27 +0100

    🔄 Reorder imports, cleanup whitespace

--- examples/vision.py
+++ examples/vision.py
@@ -1,12 +1,14 @@
-from funcchain import chain, settings
 from langchain.pydantic_v1 import BaseModel, Field
 from PIL import Image  # type: ignore
+
+from funcchain import chain, settings
 
 settings.MODEL_NAME = "gpt-4-vision-preview"
 
 
 class AnalysisResult(BaseModel):
     """The result of an image analysis."""
+
     theme: str = Field(..., description="The theme of the image")
     description: str = Field(..., description="A description of the image")
     objects: list[str] = Field(..., description="A list of objects found in the image")
@@ -22,9 +24,9 @@ def analyse_image(image: Image.Image) -> AnalysisResult:
 
 if __name__ == "__main__":
     example_image = Image.open("examples/assets/cyberspace_landscape.jpg")
-    
+
     result = analyse_image(example_image)
-    
+
     print("Theme:", result.theme)
     print("Description:", result.description)
 
