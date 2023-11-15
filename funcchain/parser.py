753e961ca5d056afb31ddef21941cb941327118d
Shroominic <db2002dominic@gmail.com>
2023-11-15 18:06:44 +0100

    🚚 Refactor ParserBaseModel, CodeBlock

--- funcchain/parser.py
+++ funcchain/parser.py
@@ -1,14 +1,13 @@
 import copy
 import json
-import re
 from typing import Callable, Optional, Type, TypeVar
 
-from langchain.output_parsers.format_instructions import PYDANTIC_FORMAT_INSTRUCTIONS
 from langchain.output_parsers.openai_functions import PydanticOutputFunctionsParser
 from langchain.schema import ChatGeneration, Generation, OutputParserException
 from langchain.schema.output_parser import BaseGenerationOutputParser, BaseOutputParser
 from pydantic.v1 import BaseModel, ValidationError
-from typing_extensions import Self
+
+from .types import ParserBaseModel, CodeBlock as CodeBlock
 
 T = TypeVar("T")
 
@@ -30,6 +29,7 @@ class LambdaOutputParser(BaseOutputParser[T]):
 
 class BoolOutputParser(BaseOutputParser[bool]):
     def parse(self, text: str) -> bool:
+        print(text)
         return text.strip()[:1].lower() == "y"
 
     def get_format_instructions(self) -> str:
@@ -87,28 +87,6 @@ class MultiToolParser(BaseGenerationOutputParser[M]):
         return PydanticOutputFunctionsParser(pydantic_schema=output_type)
 
 
-class ParserBaseModel(BaseModel):
-    @classmethod
-    def output_parser(cls) -> BaseOutputParser[Self]:
-        return CustomPydanticOutputParser(pydantic_object=cls)
-
-    @classmethod
-    def parse(cls, text: str) -> Self:
-        """Override for custom parsing."""
-        match = re.search(
-            r"\{.*\}", text.strip(), re.MULTILINE | re.IGNORECASE | re.DOTALL
-        )
-        json_str = ""
-        if match:
-            json_str = match.group()
-        json_object = json.loads(json_str, strict=False)
-        return cls.parse_obj(json_object)
-
-    @staticmethod
-    def format_instructions() -> str:
-        return PYDANTIC_FORMAT_INSTRUCTIONS
-
-
 P = TypeVar("P", bound=ParserBaseModel)
 
 
@@ -138,37 +116,3 @@ class CustomPydanticOutputParser(BaseOutputParser[P]):
     @property
     def _type(self) -> str:
         return "pydantic"
-
-
-class CodeBlock(ParserBaseModel):
-    code: str
-    language: Optional[str] = None
-
-    @classmethod
-    def parse(cls, text: str) -> "CodeBlock":
-        matches = re.finditer(
-            r"```(?P<language>\w+)?\n?(?P<code>.*?)```", text, re.DOTALL
-        )
-        for match in matches:
-            groupdict = match.groupdict()
-            groupdict["language"] = groupdict.get("language", None)
-
-            # custom markdown fix
-            if groupdict["language"] == "markdown":
-                t = text.split("```markdown")[1]
-                return cls(
-                    language="markdown",
-                    code=t[: -(len(t.split("```")[-1]) + 3)],
-                )
-
-            return cls(**groupdict)
-
-        return cls(code=text)  # TODO: fix this hack
-        raise OutputParserException("Invalid codeblock")
-
-    @staticmethod
-    def format_instructions() -> str:
-        return "Answer with a codeblock."
-
-    def __str__(self) -> str:
-        return self.code
