451d047f6b3a09446ef3c597799703abc38cc3e2
Shroominic <db2002dominic@gmail.com>
2023-11-20 15:37:54 +0100

    ✨ Add callbacks to invocations

--- funcchain/chain/invoke.py
+++ funcchain/chain/invoke.py
@@ -1,6 +1,7 @@
 from typing import TypeVar
 from langchain.schema.runnable import RunnableSequence
 from langchain.schema.chat_history import BaseChatMessageHistory
+from langchain.callbacks.base import Callbacks
 from ..utils.decorators import retry_parse, log_openai_callback
 
 T = TypeVar("T")
@@ -12,8 +13,13 @@ def invoke(
     chain: RunnableSequence[dict[str, str], T],
     memory: BaseChatMessageHistory | None,
     input: dict[str, str],
+    callbacks: Callbacks = None,
 ) -> T:  # type: ignore
-    result = chain.invoke(input)
+    result = (
+        chain.invoke(input, {"callbacks": callbacks})
+        if callbacks
+        else chain.invoke(input)
+    )
 
     if memory and isinstance(result, str):
         memory.add_ai_message(result)
@@ -27,8 +33,13 @@ async def ainvoke(
     chain: RunnableSequence[dict[str, str], T],
     memory: BaseChatMessageHistory | None,
     input: dict[str, str],
+    callbacks: Callbacks = None,
 ) -> T:
-    result = await chain.ainvoke(input)
+    result = (
+        await chain.ainvoke(input, {"callbacks": callbacks})
+        if callbacks
+        else await chain.ainvoke(input)
+    )
 
     if memory and isinstance(result, str):
         memory.add_ai_message(result)
