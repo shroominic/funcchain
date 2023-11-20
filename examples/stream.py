613314d7f45d53d654ccf047126234ca6393847d
Shroominic <db2002dominic@gmail.com>
2023-11-20 14:42:48 +0100

    🚀 Add story generator stream

---
+++ examples/stream.py
@@ -0,0 +1,15 @@
+from funcchain import chain, settings
+from funcchain.streaming import stream_to
+
+settings.MODEL_TEMPERATURE = 1
+settings.MODEL_NAME = "gpt-3.5-turbo-1106"
+
+
+def generate_story_of(topic: str) -> str:
+    """
+    Write a short story based on the topic.
+    """
+    return chain()
+
+with stream_to(print):
+    generate_story_of("a space cat")
