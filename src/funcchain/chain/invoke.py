400304a11253462e26319822c0a5aad342cb70c7
Shroominic <db2002dominic@gmail.com>
2023-11-22 21:29:43 +0100

    ✨ Refactor invoke functions

--- src/funcchain/chain/invoke.py
+++ src/funcchain/chain/invoke.py
@@ -2,7 +2,7 @@ from typing import TypeVar
 from langchain.schema.runnable import RunnableSequence
 from langchain.schema.chat_history import BaseChatMessageHistory
 from langchain.callbacks.base import Callbacks
-from ..utils.decorators import retry_parse, log_openai_callback
+from ..utils.decorators import retry_parse, log_openai_callback, get_parent_frame
 
 T = TypeVar("T")
 
@@ -15,10 +15,8 @@ def invoke(
     input: dict[str, str],
     callbacks: Callbacks = None,
 ) -> T:  # type: ignore
-    result = (
-        chain.invoke(input, {"callbacks": callbacks})
-        if callbacks
-        else chain.invoke(input)
+    result = chain.invoke(
+        input, {"run_name": get_parent_frame(5).function, "callbacks": callbacks}
     )
 
     if memory and isinstance(result, str):
@@ -35,10 +33,8 @@ async def ainvoke(
     input: dict[str, str],
     callbacks: Callbacks = None,
 ) -> T:
-    result = (
-        await chain.ainvoke(input, {"callbacks": callbacks})
-        if callbacks
-        else await chain.ainvoke(input)
+    result = await chain.ainvoke(
+        input, {"run_name": get_parent_frame(5).function, "callbacks": callbacks}
     )
 
     if memory and isinstance(result, str):
