d049d2916e05f81e334a9b0dd9b241c0b09279de
Shroominic <db2002dominic@gmail.com>
2023-11-26 13:27:23 +0100

    ✨ Refactor invoke functions

--- src/funcchain/chain/invoke.py
+++ src/funcchain/chain/invoke.py
@@ -1,8 +1,10 @@
 from typing import TypeVar
-from langchain.schema.runnable import RunnableSequence
+from langchain.schema import BaseMessage, BaseOutputParser
 from langchain.schema.chat_history import BaseChatMessageHistory
 from langchain.callbacks.base import Callbacks
+
 from ..utils.decorators import retry_parse, log_openai_callback, get_parent_frame
+from .creation import create_chain
 
 T = TypeVar("T")
 
@@ -10,16 +12,28 @@ T = TypeVar("T")
 @retry_parse
 @log_openai_callback
 def invoke(
-    chain: RunnableSequence[dict[str, str], T],
-    memory: BaseChatMessageHistory | None,
-    input: dict[str, str],
+    system: str,
+    instruction: str,
+    parser: BaseOutputParser[T],
+    context: list[BaseMessage],
+    memory: BaseChatMessageHistory,
+    input_kw: dict[str, str] = {},
     callbacks: Callbacks = None,
-) -> T:  # type: ignore
+) -> T:
+    chain = create_chain(
+        system,
+        instruction,
+        parser,
+        context,
+        memory,
+        input_kw,
+    )
     result = chain.invoke(
-        input, {"run_name": get_parent_frame(5).function, "callbacks": callbacks}
+        input_kw, {"run_name": get_parent_frame(5).function, "callbacks": callbacks}
     )
 
-    if memory and isinstance(result, str):
+    if isinstance(result, str):
+        # TODO: function calls?
         memory.add_ai_message(result)
 
     return result
@@ -28,16 +42,28 @@ def invoke(
 @retry_parse
 @log_openai_callback
 async def ainvoke(
-    chain: RunnableSequence[dict[str, str], T],
-    memory: BaseChatMessageHistory | None,
-    input: dict[str, str],
+    system: str,
+    instruction: str,
+    parser: BaseOutputParser[T],
+    context: list[BaseMessage],
+    memory: BaseChatMessageHistory,
+    input_kw: dict[str, str] = {},
     callbacks: Callbacks = None,
 ) -> T:
+    chain = create_chain(
+        system,
+        instruction,
+        parser,
+        context,
+        memory,
+        input_kw,
+    )
     result = await chain.ainvoke(
-        input, {"run_name": get_parent_frame(5).function, "callbacks": callbacks}
+        input_kw, {"run_name": get_parent_frame(5).function, "callbacks": callbacks}
     )
 
-    if memory and isinstance(result, str):
+    if isinstance(result, str):
+        # TODO: function calls?
         memory.add_ai_message(result)
 
     return result
