a42cc8ea9b1ee9d2d57b5cc13a1c11091ad1bfda
Shroominic <db2002dominic@gmail.com>
2023-12-10 18:45:23 +0100

    🔄 Refactor import statements

--- src/funcchain/utils/__init__.py
+++ src/funcchain/utils/__init__.py
@@ -3,7 +3,7 @@ from .function_frame import *  # noqa: F401, F403
 from .helpers import *  # noqa: F401, F403
 from .image import *  # noqa: F401, F403
 from .model_defaults import *  # noqa: F401, F403
-from .json_schema_to_grammar import (
+from .grammars import (
     schema_to_grammar,  # noqa: F401, F403
     pydantic_to_grammar,  # noqa: F401, F403
 )
