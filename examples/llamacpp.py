adbcab1edc0b0139d02136019e081cd4838d7871
Shroominic <db2002dominic@gmail.com>
2023-12-09 16:20:12 -0800

    🔄 Update sentiment analysis model

--- examples/llamacpp.py
+++ examples/llamacpp.py
@@ -1,24 +1,24 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
 
 from funcchain import chain, settings
 from funcchain._llms import ChatLlamaCpp
 from funcchain.streaming import stream_to
 
 
-class Keyword(BaseModel):
-    word: str
-    rating: int
+class SentimentAnalysis(BaseModel):
+    sentiment: bool = Field(description="True for Happy, False for Sad")
+    score: int = Field(description="The score of the sentiment")
+
+    @field_validator("score")
+    def check_score(cls, v: int) -> int:
+        if v < 10 or v > 100:
+            raise ValueError("Score must be between 10 and 100")
+        return v
 
 
-class Poem(BaseModel):
-    topic: str
-    content: str
-    keywords: list[Keyword]
-
-
-def generate_poem(topic: str) -> Poem:
+def analyze(topic: str) -> SentimentAnalysis:
     """
-    Generate a poem about the topic.
+    Determines the sentiment of the topic
     """
     return chain()
 
@@ -26,14 +26,15 @@ def generate_poem(topic: str) -> Poem:
 if __name__ == "__main__":
     settings.LLM = ChatLlamaCpp(
         verbose=False,
-        model_path=".models/openhermes-2.5-mistral-7b.Q4_K_M.gguf",
-        streaming=True,
-        n_ctx=2048,
+        model_path=".models/openhermes-2.5-neural-chat-7b-v3-1-7b.Q5_K_M.gguf",
+        # streaming=True,
+        n_ctx=4096,
+        # n_gpu_layers=0,
         max_tokens=512,
     )
 
     with stream_to(print):
-        poem = generate_poem("llama")
+        poem = analyze("I really like when my dog does a trick")
 
     from rich import print
 
