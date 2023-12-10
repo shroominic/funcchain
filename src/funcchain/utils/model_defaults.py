30d4c8df545cd2209cf942eddfe5faa47deb9038
Shroominic <db2002dominic@gmail.com>
2023-12-09 21:22:05 -0800

    wip

--- src/funcchain/utils/model_defaults.py
+++ src/funcchain/utils/model_defaults.py
@@ -6,17 +6,24 @@ from langchain.chat_models import (
     ChatAnthropic,
     ChatGooglePalm,
     ChatOpenAI,
-    JinaChat,
 )
 from langchain.chat_models.base import BaseChatModel
 
 from ..settings import settings
 
 
+class ModelNotFoundError(LookupError):
+    ...
+
+
+class MissingApiKeyError(ValueError):
+    ...
+
+
 def model_from_env(
     dotenv_path: str = "./.env",
     **kwargs: Any,
-) -> BaseChatModel:
+) -> BaseChatModel | None:
     """
     Automatically search your env variables for api keys
     and gives you the corresponding chat model interface.
@@ -26,7 +33,6 @@ def model_from_env(
     - AZURE_API_KEY
     - ANTHROPIC_API_KEY
     - GOOGLE_API_KEY
-    - JINACHAT_API_KEY
 
     Raises:
     - ValueError, when the model is not found.
@@ -42,55 +48,78 @@ def model_from_env(
         return ChatAnthropic(**kwargs)
     if settings.GOOGLE_API_KEY:
         return ChatGooglePalm(**kwargs)
-    if settings.JINACHAT_API_KEY:
-        return JinaChat(**kwargs)
-    if name := settings.MODEL_NAME:
-        return model_from_name(name, **kwargs)
-    raise ValueError(
-        "Model not found! Make sure to use the correct env variables."
-        # "For more info: docs.url"
-    )
+    return None
 
 
-def model_from_name(
-    model_name: str,
-    /,
-    **kwargs: Any,
+def get_gguf_model(name: str, label: str) -> str:
+    """
+    Gather GGUF model from huggingface/TheBloke
+    e.g. https://huggingface.co/TheBloke/OpenHermes-2.5-neural-chat-7B-v3-1-7B-GGUF
+
+    Raises ModelNotFound(model_name) error in case of no result.
+    """
+    # check if model already downloaded
+    # if so return None
+
+    # check if available on huggingface
+    # if not raise
+
+    # ask before downloading
+    # if not suggest other options
+
+    # download to settings.MODEL_LIBRARY_PATH
+
+    return f"{settings.MODEL_LIBRARY}/{name}_{label}.gguf"
+
+
+def univeral_model_selector(
+    model_name: str | None,
+    **model_kwargs: Any,
 ) -> BaseChatModel:
     """
-    Input model_name using this schema
+    Automatically selects the best possible model for a given ModelName.
+    You can use this schema:
 
-    "provider::model_name"
+    "provider/model_name:"
 
     and automatically select the right model for you.
     You can add optional model kwargs like temperature.
 
     Examples:
-    - "openai::gpt-3.5-turbo"
-    - "anthropic::claude-2"
+    - "openai/gpt-3.5-turbo"
+    - "anthropic/claude-2"
+    - "llamacpp/neural-chat:7b-v3.1-q8_0"
+
 
     Supported:
-        [ openai, anthropic, google, jina ]
-
-    Coming Soon:
-        [ local, open_router ]
+        [ openai, anthropic, google, llamacpp ]
 
     Raises:
-    - ValueError, when the model is not found.
+    - ModelNotFoundError, when the model is not found.
     """
-    type, name = model_name.split("::")
-    kwargs["model"] = name
+    if not model_name:
+        if model := model_from_env(**model_kwargs):
+            return model
+        raise ModelNotFoundError(
+            "Please specify a model_name or add certain env variables."
+            "You can checkout out docs for supported models: todowritedocs.com"
+        )
 
-    match type:
-        case "openai":
-            return ChatOpenAI(**kwargs)
-        case "azure":
-            return AzureChatOpenAI(**kwargs)
-        case "anthropic":
-            return ChatAnthropic(**kwargs)
-        case "google":
-            return ChatGooglePalm(**kwargs)
-    raise ValueError(
-        "Model not found! Make sure to use the correct env variables."
-        # "For more info: docs.url"
-    )
+    mtype, name_lable = model_name.split("/")
+    name, label = name_lable.split(":")
+
+    model_kwargs["model_name"] = name
+
+    if mtype:
+        match type:
+            case "openai":
+                return ChatOpenAI(**model_kwargs)
+            case "anthropic":
+                return ChatAnthropic(**model_kwargs)
+            case "google":
+                return ChatGooglePalm(**model_kwargs)
+            case "llamacpp":
+                return ChatLlamaCpp(
+                    model_path=get_gguf_model(name, label),
+                    **model_kwargs,
+                )
