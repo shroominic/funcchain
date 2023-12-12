5213737235b4661aff251179e279457a5f738ab6
Shroominic <db2002dominic@gmail.com>
2023-12-12 07:48:13 +0100

    🔁 Rename startup_names.py file

---
+++ examples/async/startup_names.py
@@ -0,0 +1,34 @@
+import asyncio
+
+from pydantic import BaseModel
+
+from funcchain import achain, settings
+
+settings.TEMPERATURE = 1
+
+
+class StartupConcept(BaseModel):
+    name: str
+    description: str
+
+
+async def startup_generator(topic: str) -> StartupConcept:
+    """
+    Generate a random startup for the given topic.
+    """
+    print("Generating startup for topic:", topic)
+    return await achain()
+
+
+async def generate_random_startups(topic: str, amount: int = 3) -> list[StartupConcept]:
+    return await asyncio.gather(*[startup_generator(topic) for _ in range(amount)])
+
+
+if __name__ == "__main__":
+    topic = "AI generated Vegan Recipes"
+
+    startups = asyncio.run(generate_random_startups(topic))
+
+    for startup in startups:
+        print("name:", startup.name)
+        print("concept:", startup.description)
