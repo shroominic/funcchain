967a79809db70a22ed2d2bd3d77d078d827e92d1
Shroominic <db2002dominic@gmail.com>
2023-12-12 07:48:13 +0100

    🔧 Update settings attribute name

--- examples/stream.py
+++ examples/stream.py
@@ -1,7 +1,7 @@
 from funcchain import chain, settings
 from funcchain.streaming import stream_to
 
-settings.MODEL_TEMPERATURE = 1
+settings.TEMPERATURE = 1
 settings.MODEL_NAME = "gpt-3.5-turbo-1106"
 settings.STREAMING = True
 
