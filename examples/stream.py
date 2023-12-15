f6d0221ea5130bde19bc42693f4083b88fac56f7
Shroominic <db2002dominic@gmail.com>
2023-12-15 10:02:01 +0100

    🔧 Update settings casing

--- examples/stream.py
+++ examples/stream.py
@@ -1,9 +1,8 @@
 from funcchain import chain, settings
 from funcchain.streaming import stream_to
 
-settings.TEMPERATURE = 1
-settings.MODEL_NAME = "gpt-3.5-turbo-1106"
-settings.STREAMING = True
+settings.temperature = 1
+settings.streaming = True
 
 
 def generate_story_of(topic: str) -> str:
