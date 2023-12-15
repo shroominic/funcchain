2321ac56b3cfefe83479898b04bb23a3be1cf274
Shroominic <db2002dominic@gmail.com>
2023-12-15 10:02:01 +0100

    🔧 Refactor settings structure

--- src/funcchain/settings.py
+++ src/funcchain/settings.py
@@ -4,62 +4,60 @@ Automatically loads environment variables from .env file
 """
 from typing import Optional, TypedDict
 
-from dotenv import load_dotenv
 from langchain.cache import InMemoryCache
 from langchain.chat_models.base import BaseChatModel
 from langchain.globals import set_llm_cache
 from langchain.schema.runnable import RunnableWithFallbacks
 from pydantic_settings import BaseSettings
-
-load_dotenv("./.env")
+from pydantic import Field
 
 set_llm_cache(InMemoryCache())
 
 
 class FuncchainSettings(BaseSettings):
-    # General
-    LLM: BaseChatModel | RunnableWithFallbacks | None = None
-    DEBUG: bool = True
+    debug: bool = True
 
-    # TODO: merge LLM and MODEL_NAME
-    MODEL_NAME: str = "openai/gpt-3.5-turbo-1106"
-    MODEL_LIBRARY: str = "./.models"
+    llm: BaseChatModel | RunnableWithFallbacks | str = Field(
+        default="openai/gpt-3.5-turbo-1106",
+        validate_default=False,
+    )
 
-    # Prompt
-    DEFAULT_SYSTEM_PROMPT: str = ""
+    local_models_path: str = "./.models"
 
-    RETRY_PARSE: int = 5
-    RETRY_PARSE_SLEEP: float = 0.1
+    default_system_prompt: str = ""
+
+    retry_parse: int = 5
+    retry_parse_sleep: float = 0.1
 
     # KEYS
-    OPENAI_API_KEY: Optional[str] = None
-    AZURE_API_KEY: Optional[str] = None
-    ANTHROPIC_API_KEY: Optional[str] = None
-    GOOGLE_API_KEY: Optional[str] = None
+    openai_api_key: Optional[str] = None
+    azure_api_key: Optional[str] = None
+    anthropic_api_key: Optional[str] = None
+    google_api_key: Optional[str] = None
 
     # MODEL KWARGS
-    VERBOSE: bool = False
-    MAX_TOKENS: int = 2048
-    TEMPERATURE: float = 0.1
-    CONTEXT_LENGTH: int = 8196
-    STREAMING: bool = False
+    verbose: bool = False
+    max_tokens: int = 2048
+    temperature: float = 0.1
+    context_lenght: int = 8196
+    streaming: bool = False
 
     def model_kwargs(self) -> dict:
         return {
-            "verbose": self.VERBOSE,
-            "temperature": self.TEMPERATURE,
-            "max_tokens": self.MAX_TOKENS,
-            "streaming": self.STREAMING,
+            "verbose": self.verbose,
+            "temperature": self.temperature,
+            "max_tokens": self.max_tokens,
+            "streaming": self.streaming,
         }
 
     def openai_kwargs(self) -> dict:
         return {
-            "openai_api_key": self.OPENAI_API_KEY,
+            "openai_api_key": self.openai_api_key,
         }
 
     def llama_kwargs(self) -> dict:
         return {
-            "n_ctx": self.CONTEXT_LENGTH,
+            "n_ctx": self.context_lenght,
         }
 
 
@@ -67,14 +65,13 @@ settings = FuncchainSettings()
 
 
 class SettingsOverride(TypedDict, total=False):
-    llm: BaseChatModel | RunnableWithFallbacks | None
-    model_name: str
+    llm: BaseChatModel | RunnableWithFallbacks | str
 
     verbose: bool
     temperature: float
     max_tokens: int
     streaming: bool
-    # context_length: int
+    # TODO: context_length: int
 
 
 def get_settings(override: Optional[SettingsOverride] = None) -> FuncchainSettings:
