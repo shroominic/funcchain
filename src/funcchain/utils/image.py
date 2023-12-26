d03bdaa345ea05d75b6399c11a0f4506ce50cadd
Shroominic <db2002dominic@gmail.com>
2023-12-26 20:48:29 +0100

    🖼️ Add base64 to image conversion

--- src/funcchain/utils/image.py
+++ src/funcchain/utils/image.py
@@ -1,4 +1,4 @@
-from base64 import b64encode
+from base64 import b64encode, b64decode
 from io import BytesIO
 
 from PIL import Image
@@ -9,3 +9,10 @@ def image_to_base64_url(image: Image.Image) -> str:
         image.save(output, format="PNG")
         base64_image = b64encode(output.getvalue()).decode("utf-8")
     return f"data:image/jpeg;base64,{base64_image}"
+
+
+def base64_url_to_image(base64_url: str) -> Image.Image:
+    base64_image = base64_url.split(",")[1]
+    image_bytes = b64decode(base64_image)
+    image = Image.open(BytesIO(image_bytes))
+    return image
