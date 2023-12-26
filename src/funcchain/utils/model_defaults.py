a622f80de7464d065b2d1feda292953c1fc84b4b
Shroominic <db2002dominic@gmail.com>
2023-12-26 20:48:30 +0100

    🔥 Refactor model selection logic

--- src/funcchain/utils/model_defaults.py
+++ src/funcchain/utils/model_defaults.py
@@ -1,88 +1,8 @@
-from pathlib import Path
 from typing import Any
 
-from langchain.chat_models import (
-    AzureChatOpenAI,
-    ChatAnthropic,
-    ChatGooglePalm,
-    ChatOpenAI,
-)
 from langchain_core.language_models import BaseChatModel
 
-from .._llms import ChatLlamaCpp
 from ..settings import FuncchainSettings
-
-
-def get_gguf_model(
-    name: str,
-    label: str,
-    settings: FuncchainSettings,
-) -> Path:
-    """
-    Gather GGUF model from huggingface/TheBloke
-
-    possible input:
-    - DiscoLM-mixtral-8x7b-v2-GGUF
-    - TheBloke/DiscoLM-mixtral-8x7b-v2
-    - discolm-mixtral-8x7b-v2
-    ...
-
-    Raises ModelNotFound(name) error in case of no result.
-    """
-    from huggingface_hub import hf_hub_download
-
-    name = name.removesuffix("-GGUF")
-    label = "Q5_K_M" if label == "latest" else label
-
-    model_path = Path(settings.local_models_path)
-
-    if (p := model_path / f"{name.lower()}.{label}.gguf").exists():
-        return p
-
-    # check if available on huggingface
-    try:
-        # check local cache
-
-        input(
-            f"Do you want to download this model from huggingface.co/TheBloke/{name}-GGUF ?\n"
-            "Press enter to continue."
-        )
-        print("\033c")
-        print("Downloading model from huggingface...")
-        p = hf_hub_download(
-            repo_id=f"TheBloke/{name}-GGUF",
-            filename=f"{name.lower()}.{label}.gguf",
-            local_dir=model_path,
-            local_dir_use_symlinks=True,
-        )
-        print("\033c")
-        return Path(p)
-    except Exception as e:
-        print(e)
-        raise ValueError(f"ModelNotFound: {name}.{label}")
-
-
-def default_model_fallback(
-    settings: FuncchainSettings,
-    **model_kwargs: Any,
-) -> ChatLlamaCpp:
-    """
-    Give user multiple options for local models to download.
-    """
-    if (
-        input("ModelNotFound: Do you want to download a local model instead?")
-        .lower()
-        .startswith("y")
-    ):
-        model_kwargs.update(settings.llama_kwargs())
-        return ChatLlamaCpp(
-            model_path=get_gguf_model(
-                "neural-chat-7b-v3-1", "Q4_K_M", settings
-            ).as_posix(),
-            **model_kwargs,
-        )
-    print("Please select a model to use funcchain!")
-    exit(0)
 
 
 def univeral_model_selector(
@@ -101,12 +21,10 @@ def univeral_model_selector(
     Examples:
     - "openai/gpt-3.5-turbo"
     - "anthropic/claude-2"
-    - "thebloke/deepseek-llm-7b-chat"
-
-    (gguf models from huggingface.co/TheBloke)
+    - "ollama/deepseek-llm-7b-chat"
 
     Supported:
-        [ openai, anthropic, google, llamacpp ]
+        [ openai, anthropic, google, ollama ]
 
     Raises:
     - ModelNotFoundError, when the model is not found.
@@ -115,12 +33,7 @@ def univeral_model_selector(
     model_kwargs.update(settings.model_kwargs())
 
     if model_name:
-        mtype, name_lable = (
-            model_name.split("/") if "/" in model_name else ("", model_name)
-        )
-        name, label = (
-            name_lable.split(":") if ":" in name_lable else (name_lable, "latest")
-        )
+        mtype, name = model_name.split("/") if "/" in model_name else ("", model_name)
         mtype = mtype.lower()
 
         model_kwargs["model_name"] = name
@@ -128,42 +41,66 @@ def univeral_model_selector(
         try:
             match mtype:
                 case "openai":
+                    from langchain.chat_models import ChatOpenAI
+
                     model_kwargs.update(settings.openai_kwargs())
                     return ChatOpenAI(**model_kwargs)
+
                 case "anthropic":
+                    from langchain.chat_models import ChatAnthropic
+
                     return ChatAnthropic(**model_kwargs)
+
                 case "google":
+                    from langchain.chat_models import ChatGooglePalm
+
                     return ChatGooglePalm(**model_kwargs)
-                case "llamacpp" | "thebloke" | "huggingface" | "local" | "gguf":
-                    model_kwargs.pop("model_name")
-                    model_path = get_gguf_model(name, label, settings).as_posix()
-                    print("Using model:", model_path)
-                    model_kwargs.update(settings.llama_kwargs())
-                    return ChatLlamaCpp(
-                        model_path=model_path,
-                        **model_kwargs,
-                    )
+
+                case "ollama":
+                    from .._llms import ChatOllama
+
+                    model = model_kwargs.pop("model_name")
+                    model_kwargs.update(settings.ollama_kwargs())
+                    return ChatOllama(model=model, **model_kwargs)
+
         except Exception as e:
             print("ERROR:", e)
             raise e
 
         try:
             if "gpt-4" in name or "gpt-3.5" in name:
+                from langchain.chat_models import ChatOpenAI
+
                 model_kwargs.update(settings.openai_kwargs())
                 return ChatOpenAI(**model_kwargs)
+
         except Exception as e:
             print(e)
 
     model_kwargs.pop("model_name")
 
     if settings.openai_api_key:
+        from langchain.chat_models import ChatOpenAI
+
         model_kwargs.update(settings.openai_kwargs())
         return ChatOpenAI(**model_kwargs)
+
     if settings.azure_api_key:
+        from langchain.chat_models import AzureChatOpenAI
+
         return AzureChatOpenAI(**model_kwargs)
+
     if settings.anthropic_api_key:
+        from langchain.chat_models import ChatAnthropic
+
         return ChatAnthropic(**model_kwargs)
+
     if settings.google_api_key:
+        from langchain.chat_models import ChatGooglePalm
+
         return ChatGooglePalm(**model_kwargs)
 
-    return default_model_fallback(**model_kwargs)
+    raise ValueError(
+        "Could not read llm selector string. "
+        "Please check [here](https://github.com/shroominic/funcchain/blob/main/MODELS.md) for more info."
+    )
