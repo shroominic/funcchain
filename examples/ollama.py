587ca6ec56c17a16388e8cf3491e6522ffbd8ab0
Shroominic <db2002dominic@gmail.com>
2023-12-26 20:48:21 +0100

    🔁 Rename ollama.py

---
+++ examples/ollama.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, Field
+
+from funcchain import chain, settings
+from funcchain.streaming import stream_to
+
+
+# define your model
+class SentimentAnalysis(BaseModel):
+    analysis: str = Field(description="A description of the analysis")
+    sentiment: bool = Field(description="True for Happy, False for Sad")
+
+
+# define your prompt
+def analyze(text: str) -> SentimentAnalysis:
+    """
+    Determines the sentiment of the text.
+    """
+    return chain()
+
+
+if __name__ == "__main__":
+    # set global llm
+    settings.llm = "ollama/wizardcoder:34b-python-q3_K_M"
+
+    # log tokens as stream to console
+    with stream_to(print):
+        # run prompt
+        poem = analyze("I really like when my dog does a trick!")
+
+    # print final parsed output
+    from rich import print
+
+    print(poem)
