2f43af26a1c591fb871c098393cfa54ddde34e30
Shroominic <db2002dominic@gmail.com>
2024-01-15 21:05:34 +0100

    ✨ Add output types exports

--- src/funcchain/syntax/__init__.py
+++ src/funcchain/syntax/__init__.py
@@ -2,9 +2,12 @@
 """
 from .decorators import runnable
 from .executable import achain, chain
+from .output_types import CodeBlock, Error
 
 __all__ = [
     "chain",
     "achain",
     "runnable",
+    "CodeBlock",
+    "Error",
 ]
