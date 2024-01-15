8fef41a213a70948175eb59c6c4c1a773495dee4
Shroominic <db2002dominic@gmail.com>
2024-01-15 15:34:38 +0100

    🔖 literal str enum example

--- examples/literals.py
+++ examples/literals.py
@@ -4,9 +4,11 @@ from funcchain import chain
 from pydantic import BaseModel
 
 
+# just a silly example to schowcase the Literal type
 class Ranking(BaseModel):
     chain_of_thought: str
     score: Literal[11, 22, 33, 44, 55]
+    error: Literal["no_input", "all_good", "invalid"]
 
 
 def rank_output(output: str) -> Ranking:
