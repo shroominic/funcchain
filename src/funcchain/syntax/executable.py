d41834e89c554e6daa50542b96fe4283f4b34e35
Shroominic <db2002dominic@gmail.com>
2024-01-22 21:24:54 +0000

    ✨ Refactor type handling, imports

--- src/funcchain/syntax/executable.py
+++ src/funcchain/syntax/executable.py
@@ -1,6 +1,5 @@
-from typing import TypeVar
+from typing import Any, TypeVar
 
-from langchain.memory import ChatMessageHistory
 from langchain_core.callbacks.base import Callbacks
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.language_models import BaseChatModel
@@ -9,6 +8,7 @@ from langchain_core.runnables import Runnable
 
 from ..backend.compiler import compile_chain
 from ..backend.meta_inspect import (
+    args_from_parent,
     from_docstring,
     get_output_type,
     get_parent_frame,
@@ -16,8 +16,7 @@ from ..backend.meta_inspect import (
 )
 from ..backend.settings import SettingsOverride, create_local_settings
 from ..schema.signature import Signature
-
-ChainOut = TypeVar("ChainOut")
+from ..utils.memory import ChatMessageHistory
 
 
 def chain(
@@ -27,27 +26,30 @@ def chain(
     memory: BaseChatMessageHistory | None = None,
     settings_override: SettingsOverride = {},
     **input_kwargs: str,
-) -> ChainOut:  # type: ignore
+) -> Any:
     """
     Generate response of llm for provided instructions.
     """
     settings = create_local_settings(settings_override)
     callbacks: Callbacks = None
     output_type = get_output_type()
+    input_args: list[tuple[str, type]] = args_from_parent()
 
     memory = memory or ChatMessageHistory()
     input_kwargs.update(kwargs_from_parent())
+
+    # todo maybe this should be done in the prompt processor?
     system = system or settings.system_prompt
     instruction = instruction or from_docstring()
 
     sig: Signature = Signature(
         instruction=instruction,
-        input_args=list(input_kwargs.keys()),
+        input_args=input_args,
         output_type=output_type,
         history=context,
         settings=settings,
     )
-    chain: Runnable[dict[str, str], ChainOut] = compile_chain(sig)
+    chain: Runnable[dict[str, str], Any] = compile_chain(sig)
     result = chain.invoke(input_kwargs, {"run_name": get_parent_frame(3).function, "callbacks": callbacks})
 
     if memory and isinstance(result, str):
@@ -64,13 +66,14 @@ async def achain(
     memory: BaseChatMessageHistory | None = None,
     settings_override: SettingsOverride = {},
     **input_kwargs: str,
-) -> ChainOut:
+) -> Any:
     """
     Asyncronously generate response of llm for provided instructions.
     """
     settings = create_local_settings(settings_override)
     callbacks: Callbacks = None
     output_type = get_output_type()
+    input_args: list[tuple[str, type]] = args_from_parent()
 
     memory = memory or ChatMessageHistory()
     input_kwargs.update(kwargs_from_parent())
@@ -81,12 +84,12 @@ async def achain(
 
     sig: Signature = Signature(
         instruction=instruction,
-        input_args=list(input_kwargs.keys()),
+        input_args=input_args,
         output_type=output_type,
         history=context,
         settings=settings,
     )
-    chain: Runnable[dict[str, str], ChainOut] = compile_chain(sig)
+    chain: Runnable[dict[str, str], Any] = compile_chain(sig)
     result = await chain.ainvoke(input_kwargs, {"run_name": get_parent_frame(5).function, "callbacks": callbacks})
 
     if memory and isinstance(result, str):
@@ -94,6 +97,9 @@ async def achain(
         memory.add_ai_message(result)
 
     return result
+
+
+ChainOut = TypeVar("ChainOut")
 
 
 def compile_runnable(
@@ -113,10 +119,11 @@ def compile_runnable(
     instruction = "\n" + instruction
     settings = create_local_settings(settings_override)
     context = [SystemMessage(content=system)] + context
+    _input_args: list[tuple[str, type]] = [(arg, str) for arg in input_args]
 
     sig: Signature = Signature(
         instruction=instruction,
-        input_args=input_args,
+        input_args=_input_args,
         output_type=output_type,
         history=context,
         settings=settings,
