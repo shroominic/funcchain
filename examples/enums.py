05281735d43ef2a581d7fa5c71848d69532fbd7a
Shroominic <db2002dominic@gmail.com>
2024-01-22 22:05:16 +0000

    🔥 Remove settings dependency

--- examples/enums.py
+++ examples/enums.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from funcchain import chain, settings
+from funcchain import chain
 from pydantic import BaseModel
 
 
@@ -21,6 +21,4 @@ def make_decision(question: str) -> Decision:
 
 
 if __name__ == "__main__":
-    settings.llm = "ollama/phi-2"
-
     print(make_decision("Do you like apples?"))
