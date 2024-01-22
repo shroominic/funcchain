d39ab032ab39e6c26eea4088b62e566c41471514
Shroominic <db2002dominic@gmail.com>
2024-01-22 21:24:54 +0000

    ✨ Add TODO comments

--- src/funcchain/syntax/input_types.py
+++ src/funcchain/syntax/input_types.py
@@ -3,12 +3,14 @@ from typing import TypedDict
 from langchain_core.chat_history import BaseChatMessageHistory
 
 
+# TODO: implement
 class ImageURL(TypedDict):
     """Funcchain type for passing an image as external url."""
 
     url: str
 
 
+# TODO: implement
 class ChatHistory(BaseChatMessageHistory):
     """Funcchain Type Wrapper for detecting ChatHistorys."""
 
