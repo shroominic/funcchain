8808a474b13570144308bd1b1241d4d62072edc5
Shroominic <db2002dominic@gmail.com>
2024-02-01 14:14:45 +0400

    ✨ Add runnable decorator

--- src/funcchain/__init__.py
+++ src/funcchain/__init__.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 
 from .backend.settings import settings
+from .syntax.decorators import runnable
 from .syntax.executable import achain, chain
 from .syntax.input_types import Image
 from .syntax.output_types import Error
@@ -9,6 +10,7 @@ __all__ = [
     "settings",
     "chain",
     "achain",
+    "runnable",
     "BaseModel",
     "Image",
     "Error",
