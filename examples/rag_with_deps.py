37b6b149dbf365393e5763c6aa05256c04efdd5b
Shroominic <db2002dominic@gmail.com>
2024-02-15 14:03:36 +0800

    ✨ Add RAG example script

---
+++ examples/rag_with_deps.py
@@ -0,0 +1,34 @@
+from operator import itemgetter
+from typing import Annotated
+
+from funcchain.syntax import chain, runnable
+from funcchain.syntax.params import Depends
+from langchain_community.vectorstores.faiss import FAISS
+from langchain_openai.embeddings import OpenAIEmbeddings
+
+retriever = FAISS.from_texts(
+    [
+        "cold showers are good for your immune system",
+        "i dont like when people are mean to me",
+        "japanese tea is full of heart warming flavors",
+    ],
+    embedding=OpenAIEmbeddings(),
+).as_retriever(
+    search_kwargs={"k": 1},
+)
+
+
+@runnable
+def poem_with_retrieval(
+    topic: str,
+    context: Annotated[str, Depends(itemgetter("topic") | retriever)] = "N/A",
+) -> str:
+    """
+    Generate a poem about the topic with the given context.
+    """
+    return chain()
+
+
+print(
+    poem_with_retrieval.invoke({"topic": "love"}),
+)
