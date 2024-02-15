ab8a9ea864390efacbb72388e099b3cb9000526a
Shroominic <db2002dominic@gmail.com>
2024-02-15 13:29:11 +0800

    🚀 Add new GPT models

--- src/funcchain/model/abilities.py
+++ src/funcchain/model/abilities.py
@@ -7,6 +7,8 @@ verified_openai_function_models = [
     "gpt-4",
     "gpt-4-0613",
     "gpt-4-1106-preview",
+    "gpt-4-0106-preview",
+    "gpt-4-turbo-preview",
     "gpt-4-32k",
     "gpt-4-32k-0613",
     "gpt-3.5-turbo",
