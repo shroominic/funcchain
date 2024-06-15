38dcee45dd28f4cc53b6feab39cee8bf390f8bcb
Shroominic <db2002dominic@gmail.com>
2024-06-15 14:25:27 -0700

    add groq example for testing

---
+++ examples/models/groq_models.py
@@ -0,0 +1,29 @@
+from funcchain import chain, settings
+from langchain_groq import ChatGroq
+from pydantic import BaseModel, Field
+from rich import print
+
+
+class SentimentAnalysis(BaseModel):
+    analysis: str = Field(description="A description of the analysis")
+    sentiment: bool = Field(description="True for Happy, False for Sad")
+
+
+def analyze(text: str) -> SentimentAnalysis:
+    """
+    Determines the sentiment of the text.
+    """
+    return chain()
+
+
+if __name__ == "__main__":
+    # set global llm
+    settings.llm = ChatGroq(model="llama3-70b-8192")
+    # log tokens as stream to console
+    settings.console_stream = True
+
+    # run prompt
+    result = analyze("I really like when my dog does a trick!")
+
+    # show final parsed output
+    print(result)
