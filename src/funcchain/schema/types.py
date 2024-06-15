8fc4c09792f0b0d261e4abfd923ce75ada7f0932
Shroominic <db2002dominic@gmail.com>
2024-06-15 14:51:31 -0700

    ↗️ update chat handler type

--- src/funcchain/schema/types.py
+++ src/funcchain/schema/types.py
@@ -1,14 +1,12 @@
-from typing import Callable, Optional, Union
+from typing import Callable, Union
 
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.language_models import BaseChatModel
-from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.messages import AIMessage, BaseMessage
 from langchain_core.runnables import Runnable
 
 UniversalChatModel = Union[BaseChatModel, str, None]
 
-ChatHandler = Runnable[HumanMessage, AIMessage]
+ChatRunnable = Runnable[list[BaseMessage], AIMessage]
 
 ChatHistoryFactory = Callable[..., BaseChatMessageHistory]
-
-OptionalChatHistoryFactory = Optional[ChatHistoryFactory]
